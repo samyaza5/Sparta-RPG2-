a9aa583215cc6dfb4dd4f002f72170b0c5c1811a
Hyun149 <rusia4527@Gmail.com>
2025-04-22 09:25:25 +0900

    자동 생성 파일 추적 제거 및 정리

--- Sparta-RPG2-/obj/Debug/net8.0/Sparta-RPG2-.AssemblyInfo.cs
+++ Sparta-RPG2-/obj/Debug/net8.0/Sparta-RPG2-.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sparta-RPG2-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dff338ae6641d5d1547796ee41bc54507d9b28dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+904b55aae6d2badf57bc8b3fb34164233f661d4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sparta-RPG2-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sparta-RPG2-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
